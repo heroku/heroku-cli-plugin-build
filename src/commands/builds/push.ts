6b0a43aceb046d11151458824d17a46795740a7a
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-05-11 11:23:36 -0700

    wip

--- src/commands/builds/push.ts
+++ src/commands/builds/push.ts
@@ -4,28 +4,42 @@ import * as execa from 'execa'
 
 export default class Push extends Command {
   static aliases = ['push']
-  static description = 'deploy code to heroku'
+  static description = 'deploy code to Heroku'
 
   static flags = {
-    help: flags.help({char: 'h'})
+    help: flags.help({char: 'h'}),
+    branch: flags.string({char: 'b', description: 'local branch to push', default: 'master', required: true}),
   }
 
-  static args = [{name: 'file'}]
-
   async run() {
-    const {args, flags} = this.parse(Push)
-
+    const {flags} = this.parse(Push)
+    if (!this.heroku.auth) await this.heroku.login()
     if (await this.dirty()) {
       this.warn(`dirty working tree\nSome files have been modified that are not committed to the git repository. See details with ${color.cmd('git status')}`)
     }
+
+    await this.push(flags.branch)
   }
 
-  async dirty() {
-    let status = await this.git(['status', '--porcelain'])
+  private async push(branch: string) {
+    const auth = this.heroku.auth
+    if (!auth) return this.error('not logged in')
+    this.debug('git %o', ['-c', 'credential.https://git.heroku.com.helper=! heroku git:credentials', 'push', 'heroku', `${branch}:master`])
+    const cmd = execa('git', ['-c', 'credential.https://git.heroku.com.helper=! heroku git:credentials', 'push', 'heroku', `${branch}:master`], {
+      stdio: [0, 'pipe', 2]
+    })
+    cmd.stdout.on('data', d => {
+      console.dir(d)
+    })
+    await cmd
+  }
+
+  private async dirty() {
+    const status = await this.git(['status', '--porcelain'])
     return status !== ''
   }
 
-  async git(args: string[]): Promise<string> {
+  private async git(args: string[]): Promise<string> {
     this.debug('git %o', args)
     try {
       return await execa.stdout('git', args)
@@ -36,9 +50,11 @@ export default class Push extends Command {
 
     git branch --set-upstream-to=origin/${branch} ${branch}
 `)
-      } else if (err.message.includes('fatal: not a git repository')) {
+      }
+      if (err.message.includes('fatal: not a git repository')) {
         this.error(`Not inside a git repository\nheroku push requires that you are in a git repository.\nCurrent path: ${process.cwd()}`)
-      } else throw err
+      }
+      throw err
     }
   }
 }
