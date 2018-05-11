5b0e434230ca7881f8a31fa80aa8473c6dc35f74
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-05-11 11:14:09 -0700

    some error messaging

--- src/commands/builds/push.ts
+++ src/commands/builds/push.ts
@@ -1,21 +1,13 @@
-import {Command, flags} from '@oclif/command'
+import color from '@heroku-cli/color'
+import {Command, flags} from '@heroku-cli/command'
+import * as execa from 'execa'
 
 export default class Push extends Command {
   static aliases = ['push']
-  static description = 'describe the command here'
-
-  static examples = [
-    `$ oclif-example hello
-hello world from ./src/hello.ts!
-`,
-  ]
+  static description = 'deploy code to heroku'
 
   static flags = {
-    help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
+    help: flags.help({char: 'h'})
   }
 
   static args = [{name: 'file'}]
@@ -23,10 +15,30 @@ hello world from ./src/hello.ts!
   async run() {
     const {args, flags} = this.parse(Push)
 
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from ./src/commands/hello.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+    if (await this.dirty()) {
+      this.warn(`dirty working tree\nSome files have been modified that are not committed to the git repository. See details with ${color.cmd('git status')}`)
+    }
+  }
+
+  async dirty() {
+    let status = await this.git(['status', '--porcelain'])
+    return status !== ''
+  }
+
+  async git(args: string[]): Promise<string> {
+    this.debug('git %o', args)
+    try {
+      return await execa.stdout('git', args)
+    } catch (err) {
+      if (err.message.includes('fatal: no upstream configured for branch')) {
+        let [, branch] = err.message.match(/fatal: no upstream configured for branch '(.*)'/)
+        this.error(`${err.message}\nIf you wish to set tracking information for this branch to origin/${branch} you can do so with:
+
+    git branch --set-upstream-to=origin/${branch} ${branch}
+`)
+      } else if (err.message.includes('fatal: not a git repository')) {
+        this.error(`Not inside a git repository\nheroku push requires that you are in a git repository.\nCurrent path: ${process.cwd()}`)
+      } else throw err
     }
   }
 }
